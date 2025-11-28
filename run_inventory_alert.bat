79ef445a4bf91174453aaadc3e1f2997fef3be69
GOLDMONDO <goldmondo@outlook.com>
2025-11-27 19:17:12 -0600

    Update directory path in run_inventory_alert.bat

--- run_inventory_alert.bat
+++ run_inventory_alert.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "C:\Users\Nico Aldazaba\PycharmProjects\RemoteJobGo\Github Project\inventory_alert_system"
+cd /d "C:\Users\xxxxxxxxxx\PycharmProjects\RemoteJobGo\Github Project\inventory_alert_system"
 call ".venv\Scripts\activate.bat"
 python run.py
 pause
